5079ffcc234bda78a12c2729c81cbb72e0e0a2b0
David Ansermino <david@chainsafe.io>
2020-09-29 08:46:51 -0400

    Adds additional metrics

--- metrics/types/prometheus.go
+++ metrics/types/prometheus.go
@@ -11,14 +11,24 @@ import (
 
 type ChainMetrics struct {
 	BlocksProcessed prometheus.Counter
+	LatestProcessedBlock prometheus.Counter
+	LatestKnownBlock prometheus.Counter
 	VotesSubmitted  prometheus.Counter
 }
 
 func NewChainMetrics(chain string) *ChainMetrics {
 	metrics := &ChainMetrics{
 		BlocksProcessed: prometheus.NewCounter(prometheus.CounterOpts{
-			Name: fmt.Sprintf("%s_latest_block", chain),
+			Name: fmt.Sprintf("%s_blocks_processed", chain),
 			Help: "Number of blocks processed by the chain's listener",
+		}),
+		LatestProcessedBlock: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: fmt.Sprintf("%s_latest_processed_block", chain),
+			Help: "Latest block processed by listener",
+		}),
+		LatestKnownBlock: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: fmt.Sprintf("%s_latest_known_block", chain),
+			Help: "Latest block the listener has seen",
 		}),
 		VotesSubmitted: prometheus.NewCounter(prometheus.CounterOpts{
 			Name: fmt.Sprintf("%s_votes_submitted", chain),
@@ -27,6 +37,8 @@ func NewChainMetrics(chain string) *ChainMetrics {
 	}
 
 	prometheus.MustRegister(metrics.BlocksProcessed)
+	prometheus.MustRegister(metrics.LatestProcessedBlock)
+	prometheus.MustRegister(metrics.LatestKnownBlock)
 	prometheus.MustRegister(metrics.VotesSubmitted)
 
 	return metrics
